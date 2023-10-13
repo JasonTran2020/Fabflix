f16fc5c8332fb3a4d8f9e93ac1462b48c1358d5e
changer25 <changec0205@gmail.com>
2023-10-13 16:48:50 -0700

    finally made single-movie page workable by readding getParameterByName, fixing url AGAIN in line 81 and adding (current-day) line 75

--- WebContent/single-movie.js
+++ WebContent/single-movie.js
@@ -1,24 +1,53 @@
+/**
+ * This example is following frontend and backend separation.
+ *
+ * Before this .js is loaded, the html skeleton is created.
+ *
+ * This .js performs three steps:
+ *      1. Get parameter from request URL so it know which id to look for
+ *      2. Use jQuery to talk to backend API to get the json data.
+ *      3. Populate the data to correct html elements.
+ */
 
+
+/**
+ * Retrieve parameter from request URL, matching by parameter name
+ * @param target String
+ * @returns {*}
+ */
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    // Return the decoded parameter value
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 function handleMovieResult(resultData){
     console.log("Hey, the success callback function was called");
+    let movieName = jQuery("#movie_name");
+    movieName.append("<strong>" + resultData[0]["movie_title"] + "</strong>" + ' ('+resultData[0]["rating"]);
 
-    let testBodyElement = jQuery("#test_body");
-
-    let jsonObject=resultData[i];
     let entryHTML = "";
 
+    let testBodyElement = jQuery("#movie_info")
     //Breaking down the stars array of json objects into a single string of their names
-    let stars = jsonObject["stars"];
-    let genres = jsonObject["genres"]
+    let stars = resultData[0]["stars"];
+    let genres = resultData[0]["genres"]
     let starString = getList(stars,"name");
     let genreString= getList(genres,"name");
 
-    entryHTML +="<dt>" + jsonObject["title"] + ' ('+jsonObject["rating"] + ')' + "<dt/>";
-    entryHTML += "<dd>" + 'Directed by: ' + jsonObject["director"] + "</dd>";
-    entryHTML += "<dd>" + 'Release date: ' + jsonObject["year"] + "</dd>";
-    entryHTML += "<dd>" + 'Rating: ' + jsonObject["rating"] + "<dd/>";
-    entryHTML += "<dd>" + 'Starring: ' + starString + "<dd/>";
+    entryHTML += "<dd>" + 'Directed by: ' + resultData[0]["movie_director"] + "</dd>";
+    entryHTML += "<dd>" + 'Release date: ' + resultData[0]["movie_year"] + "</dd>";
     entryHTML += "<dd>" + 'Genres: ' + genreString + "<dd/>";
+    entryHTML += "<dd>" + 'Starring: ' + starString + "<dd/>";
 
 
     testBodyElement.append(entryHTML);
@@ -43,12 +72,12 @@ function getList(theList, propertyName){
 
 }
 
-
+let movieId = getParameterByName('id');
 
 // Makes the HTTP GET request and registers on success callback function handleMovieListResult
 jQuery.ajax({
     dataType: "json", // Setting return data type
     method: "GET", // Setting request method
-    url: "api/single-movie", // Setting request url, which is mapped by MovieListSerLet in MovieListSerLet.java
+    url: "api/single-movie?id=" + movieId, // Setting request url, which is mapped by MovieListSerLet in MovieListSerLet.java
     success: (resultData) => handleMovieResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
 });
