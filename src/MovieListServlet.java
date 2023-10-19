5f74901d40fc2bdc1c27d3f61d0e87fe72dc72a6
Jason Tran <jasont787@yahoo.com>
2023-10-18 22:10:22 -0700

    Added a new servlet to handle searching various properties of a movie. Most features implemented, but only partially tested. Also refactored a bunch of code into static methods due to the high amount of reusage between MovieSearch and MovieList servlets

--- src/MovieListServlet.java
+++ src/MovieListServlet.java
@@ -13,10 +13,7 @@ import javax.sql.DataSource;
 import javax.xml.transform.Result;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 @WebServlet(name = "MovieListServlet",urlPatterns = "/api/top-20-movie-list")
 public class MovieListServlet extends HttpServlet {
@@ -49,65 +46,25 @@ public class MovieListServlet extends HttpServlet {
             // Declare our general statement which we'll use to gather all the movies with the top 20 ratings
             Statement statement = conn.createStatement();
 
-            // We also need to gather 3 stars and 3 genres for each movie. Unlike ratings and movies, which have a 1 to 1 relationship, genres and stars both
-            //have a many-to-many relationship. I don't think it's worth creating one giant query that has everything
-            //However, that stuff was refactored to the two private functions with names that sound like what they are suppose to do. I'm losing it
-            PreparedStatement genreStatement = conn.prepareStatement("SELECT g.id,g.name FROM genres AS g, genres_in_movies AS gim WHERE gim.movieid = ? AND gim.genreid = g.id");
-            PreparedStatement starsStatement = conn.prepareStatement("SELECT s.id,s.name,s.birthYear FROM stars AS s, stars_in_movies AS sim WHERE sim.movieid = ? and sim.starid = s.id");
-
             //Get top 20 movies by joining the movies and ratings table by movieId from ratings and id from movies.
             //And then order by descending and limiting by 20
             String query = "SELECT * FROM movies AS m, ratings AS r WHERE (r.movieId = m.id) ORDER BY (r.rating) DESC LIMIT 20";
 
             ResultSet resultSet = statement.executeQuery(query);
 
-            //Conver stuff from resultSet into jsonArray because we said the content type we were going to do that
-            JsonArray jsonArray = new JsonArray();
 
-            //ResultSet holds a cursor pointing to BEFORE the first row initially, hence why we'll still get the first row even though the
-            //while loop calls .next() first, which btw does what it sounds like it does to the cursor. It will return false if there are no more rows
-            //Apparently some ResultSets are "scrollable" or "updatable", but it's safe to bet that we can at least go forward through the whole thing only once
-            //In our case, we don't need more than that
-            while (resultSet.next()){
-                String movie_id = resultSet.getString("id");
-                String title = resultSet.getString("title");
-                int year = resultSet.getInt("year");
-                String director =resultSet.getString("director");
-                float rating = resultSet.getFloat("rating");
 
-                //Add arguments to the statement before executing
-                genreStatement.setString(1, movie_id);
-                starsStatement.setString(1,movie_id);
-
-                ResultSet genreResultSet = genreStatement.executeQuery();
-                ResultSet starsResultSet = starsStatement.executeQuery();
-
-                JsonObject jsonMovieObject = new JsonObject();
-                jsonMovieObject.addProperty("movie_id", movie_id);
-                jsonMovieObject.addProperty("title",title);
-                jsonMovieObject.addProperty("year",year);
-                jsonMovieObject.addProperty("director",director);
-                jsonMovieObject.addProperty("rating",rating);
-
-                //Adding the genres was relegated to a separate function
-                this.addGenres(movie_id,jsonMovieObject,genreResultSet);
-
-                //Same goes for the stars
-                this.addStars(movie_id,jsonMovieObject,starsResultSet);
-
-                // STILL NEED TO CLOSE THE genre and stars resultSets here! They are created in each iteration and are not needed for the next!
-                genreResultSet.close();
-                starsResultSet.close();
-
-                jsonArray.add(jsonMovieObject);
-            }
+            JsonArray jsonArray = buildMovieJsonArray(resultSet,conn);
 
             // Log to localhost log
             req.getServletContext().log("getting " + jsonArray.size() + " results");
+            //Write out json response
             out.write(jsonArray.toString());
-            //Close the prepared statements
-            genreStatement.close();
-            starsStatement.close();
+
+            //Close the statement for finding the movies
+            statement.close();
+            //Close the resultset
+            resultSet.close();
             // Set response status to 200 (OK)
             resp.setStatus(200);
 
@@ -123,11 +80,10 @@ public class MovieListServlet extends HttpServlet {
 
         }
         finally {
-
             out.close();
         }
     }
-    private void addGenres(String movie_id,JsonObject jsonMovieObject, ResultSet genreResultSet) throws java.sql.SQLException{
+    public static void addGenres(String movie_id,JsonObject jsonMovieObject, ResultSet genreResultSet) throws java.sql.SQLException{
         //Add all the associated movieGenres as a JsonArray, since there are a variable amount of them per list
         JsonArray movieGenres = new JsonArray();
         while (genreResultSet.next()){
@@ -144,7 +100,7 @@ public class MovieListServlet extends HttpServlet {
 
     }
 
-    private void addStars(String movie_id,JsonObject jsonMovieObject, ResultSet starsResultSet) throws java.sql.SQLException{
+    public static void addStars(String movie_id,JsonObject jsonMovieObject, ResultSet starsResultSet) throws java.sql.SQLException{
         //Time to add the stars associated with the movie, also as a JsonArray
         JsonArray movieStars= new JsonArray();
         while (starsResultSet.next()){
@@ -159,6 +115,67 @@ public class MovieListServlet extends HttpServlet {
         }
         //Now we get to add the stars as a Json array
         jsonMovieObject.add("stars",movieStars);
+
+    }
+
+    public static JsonArray buildMovieJsonArray(ResultSet movies, Connection conn) throws SQLException {
+        //This method was created to deal with the conversion of SQL data to JsonArray AND because the MovieSearchServlet does a similar thing, so to avoid
+        //copied code, this is all going into a static method. Structurally it work make more sense for MovieSearchServlet to inherit from MovieListServlet or we put all these static methods
+        //in a separate class, but I'm getting really tired right now
+
+        // We also need to gather 3 stars and 3 genres for each movie. Unlike ratings and movies, which have a 1 to 1 relationship, genres and stars both
+        //have a many-to-many relationship. I don't think it's worth creating one giant query that has everything
+        //However, that stuff was refactored to the two private functions with names that sound like what they are suppose to do. I'm losing it
+        PreparedStatement genreStatement = conn.prepareStatement("SELECT g.id,g.name FROM genres AS g, genres_in_movies AS gim WHERE gim.movieid = ? AND gim.genreid = g.id");
+        PreparedStatement starsStatement = conn.prepareStatement("SELECT s.id,s.name,s.birthYear FROM stars AS s, stars_in_movies AS sim WHERE sim.movieid = ? and sim.starid = s.id");
+        JsonArray jsonArray = new JsonArray();
+        try{
+            //Convert stuff from resultSet into jsonArray because we said the content type we were going to do that
+            //ResultSet holds a cursor pointing to BEFORE the first row initially, hence why we'll still get the first row even though the
+            //while loop calls .next() first, which btw does what it sounds like it does to the cursor. It will return false if there are no more rows
+            //Apparently some ResultSets are "scrollable" or "updatable", but it's safe to bet that we can at least go forward through the whole thing only once
+            //In our case, we don't need more than that
+            while (movies.next()){
+                String movie_id = movies.getString("id");
+                String title = movies.getString("title");
+                int year = movies.getInt("year");
+                String director =movies.getString("director");
+                float rating = movies.getFloat("rating");
+
+                //Add arguments to the statement before executing
+                genreStatement.setString(1, movie_id);
+                starsStatement.setString(1,movie_id);
+
+                ResultSet genreResultSet = genreStatement.executeQuery();
+                ResultSet starsResultSet = starsStatement.executeQuery();
+
+                JsonObject jsonMovieObject = new JsonObject();
+                jsonMovieObject.addProperty("movie_id", movie_id);
+                jsonMovieObject.addProperty("title",title);
+                jsonMovieObject.addProperty("year",year);
+                jsonMovieObject.addProperty("director",director);
+                jsonMovieObject.addProperty("rating",rating);
+
+                //Adding the genres was relegated to a separate function
+                MovieListServlet.addGenres(movie_id,jsonMovieObject,genreResultSet);
+
+                //Same goes for the stars
+                MovieListServlet.addStars(movie_id,jsonMovieObject,starsResultSet);
+
+                // STILL NEED TO CLOSE THE genre and stars resultSets here! They are created in each iteration and are not needed for the next!
+                genreResultSet.close();
+                starsResultSet.close();
+
+                jsonArray.add(jsonMovieObject);
+            }
+
+        }
+        finally {
+            genreStatement.close();
+            starsStatement.close();
+        }
+
+        return jsonArray;
 
     }
 }
---
+++ src/MovieSearchServlet.java
@@ -0,0 +1,208 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet(name = "MovieSearchServlet",urlPatterns = "/api/search-movie")
+public class MovieSearchServlet extends HttpServlet {
+    //Lastest serialVersionUID was 4L, so bumping this up to 5L.
+    private static final long serialVersionUID = 5L;
+    public static final String TAG = "MovieSearchServlet";
+    // Create a dataSource which registered in web.xml
+    private DataSource dataSource;
+    private HttpRequestAttribute<String> titleAttribute;
+    //int is a primitve type that isn't what you would consider an Object. Integer is a wrapper class to make int into an Object
+    //UPDATE. You can just make it a string. Causes less problems anyways
+    private HttpRequestAttribute<String> yearAttribute;
+    private HttpRequestAttribute<String> directorAttribute;
+    //Would make sense to be able to search for multiple stars, but rubric seems to just ask for one. Might change later
+    //private List<HttpRequestAttribute<String>> starsAttribute;
+    private HttpRequestAttribute<String> starAttribute;
+
+    public void init(ServletConfig config) {
+        //Initialize RequestAttributes here with new objects.
+        titleAttribute = new HttpRequestAttribute<>(String.class,"title");
+        yearAttribute = new HttpRequestAttribute<>(String.class,"year");
+        directorAttribute = new HttpRequestAttribute<>(String.class,"director");
+        starAttribute = new HttpRequestAttribute<>(String.class,"star");
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("application/json");// Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = resp.getWriter();
+
+
+
+        try (Connection conn = dataSource.getConnection()){
+            // Vast majority of this was dervied from MovietListServlet. Check for more detailed comments. Comments here are more specific to searching
+
+            //Build out a query and arguments using parameters from the HttpServletRequest which may contain the title, year, director, and/or star
+            //Making a prepare statement as the arguments come from a user with potential malicious intent of using SQL injection
+            PreparedStatement statement = buildSearchPrepareStatement(req,conn);
+  
+            ResultSet resultSet = statement.executeQuery();
+
+            //Convert stuff from resultSet into jsonArray because we said the content type we were going to do that
+            JsonArray jsonArray = MovieListServlet.buildMovieJsonArray(resultSet,conn);
+
+            // Log to localhost log
+            req.getServletContext().log("getting " + jsonArray.size() + " results");
+            out.write(jsonArray.toString());
+            //Close the prepared statements
+
+            statement.close();
+            //Close the resultset
+            resultSet.close();
+            // Set response status to 200 (OK)
+            resp.setStatus(200);
+
+        }
+        catch (Exception e){
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Set response status to 500 (Internal Server Error)
+            resp.setStatus(500);
+
+        }
+        finally {
+            out.close();
+        }
+
+    }
+
+
+    //We return a List of Objects, the base class for all other classes in Java
+    //The type List<?> is a valid syntax, BUT, it means something different. It essentially means the list can be any type, BUT ONLY 1 TYPE. As in it can be List<String>, List<int>, BUT NOT List<String and int>
+    //List<Object> is what enables the latter
+    //UPDATE: Yea not going that way anymore, but leaving the comments here because it's useful to learn the diff between ? and Object
+    private PreparedStatement buildSearchPrepareStatement(HttpServletRequest request,Connection conn) throws SQLException {
+        //This method will return a string query meant to be used in a prepared statement, and a list of string arguments also meant to be used in the same prepared statement
+        String title = titleAttribute.get(request);
+        String year = yearAttribute.get(request);
+        String director = directorAttribute.get(request);
+        String star = starAttribute.get(request);
+        //boolean first = true;
+        ArrayList<String> args = new ArrayList<>();
+        String result;
+        //Assumption that star is null, in which we don't need to
+        if (star==null){
+            result= "SELECT * FROM movies AS m, ratings AS r WHERE (r.movieId = m.id) ";
+        }
+        //star is not NULL
+        else {
+            result = "SELECT * FROM movies AS m, ratings AS r, stars AS s, stars_in_movies as SIM WHERE (r.movieId = m.id) AND (sim.starId = s.id) AND (sim.movieId = m.id) ";
+            result += " AND " + this.buildLikeQueryString(star,"s.name", args);
+            //first = false;
+        }
+
+        //LIKE is case sensitive, ILIKE is case insensitive. Think ILIKE is more reasonable in this case
+        if (title!=null /*&& first*/){
+            result += " AND " + this.buildLikeQueryString(title,"m.title", args);
+            //first = false;
+        }
+//        else if (title!=null){
+//            result+= " AND " + this.buildLikeQueryString(title,"m.title");;
+//        }
+
+
+        if (director != null){
+            result += " AND " + this.buildLikeQueryString(director, "m.director",args);
+        }
+
+        if (year != null /*&& first*/){
+            result += " AND ( m.year = ?" + ") ";
+            //Manually add year here as we don't use the buildLikeQueryString method here
+            args.add(year);
+            //first = false;
+        }
+//        else if(year!=null){
+//            result += " AND m.year = " + year.toString();
+//        }
+        //Terminate the statement
+        result +=" ORDER BY (r.rating) DESC;";
+        request.getServletContext().log(TAG + "The complete SQL statement is \"" + result + "\"");
+        PreparedStatement statement = conn.prepareStatement(result);
+        //Making a prepare statement as the arguments come from a user with potential malicious intent of using SQL injection
+
+        for (int x = 0; x < args.size(); x++){
+            //The only argument in this whole statement that would NOT be a string is the year, which will have a special if statement to use setInt instead
+            //setString works locally, BUT, there is no assruance that it will work with other db drivers, so let's just be careful and use setInt
+            if (x == args.size()-1 && year !=null){
+                statement.setInt(x+1,Integer.parseInt(args.get(x)));
+            }
+            //Remember, SQL is 1-based index
+            statement.setString(x+1, args.get(x));
+        }
+        return statement;
+
+    }
+
+    private String buildLikeQueryString(String argument, String columnName, ArrayList<String> args){
+        //Ok, why are we storing the args in a parameter in stead of just sticking it into a string? Well, it's for SQL injection security
+        //Quite certain that the setArg on a prepared statement is safer than directly building a query from a string.
+        //This function will take a string in, split it into words, and create a LIKE sql clause in parentheses
+        // On MySql8, ILIKE does NOT exist. However
+        StringBuilder result = new StringBuilder("( ");
+        boolean first = true;
+        // \s means whitespace, but \ means an escape character, so we need \\s. the + means 1 or more, which is what we want
+        String[] words =argument.split("\\s+");
+        for (String word : words) {
+            if (!first) {
+                result.append(" OR ");
+            }
+            //Appending a "?" instead of the argument directly for safety
+            result.append(" ").append(columnName).append(" LIKE ").append("?").append(" ");
+            //So that next iteration there is OR
+            first = false;
+            //Store the argument separately and also add the wildcard characters here, rather than when setting the arguments in the prepared statement
+            // (because year doesn't deserve wildcards)
+            args.add("%" + word + "%");
+
+        }
+        result.append(")");
+        return result.toString();
+
+    }
+
+    class HttpRequestAttribute<T>{
+        private final Class<T> type;
+        private final String name;
+        HttpRequestAttribute(Class<T> type, String name) {
+            this.type = type;
+            this.name = name;
+        }
+
+        T get(HttpServletRequest request){
+            //PARAMETER, not attribute. Apparently attribute it server side stuff, and parameter is the stuff you see at the end or a URL
+            return type.cast(request.getParameter(name));
+        }
+    }
+
+
+}
