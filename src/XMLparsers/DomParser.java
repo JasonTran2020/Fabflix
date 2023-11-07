9395a57e0d8b646ed6149ea1dfd450d0739b7497
Jason Tran <jasont787@yahoo.com>
2023-11-07 00:30:56 -0800

    Modified getting text to use the getTextContent method from nodes instead of getNodeValue

--- src/XMLparsers/DomParser.java
+++ src/XMLparsers/DomParser.java
@@ -4,6 +4,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,13 +17,12 @@ abstract public class DomParser {
         //Only take a single value, even if there are multiple
         //First element might not have text, so keep checking all children until a text is found
         for (int i = 0; i < nodeList.getLength(); i++){
-            //Apparently text is considered its own node? hence why you need to call getFirstChild.
-            Node currentNode = nodeList.item(i).getFirstChild();
-            if (currentNode !=null){
-                textVal = currentNode.getNodeValue();
-                break;
+            if (nodeList.item(i).getFirstChild() == null){
+                continue;
             }
-
+            //Use getTextContent, instead of getNodeValue, as it gets the text of the entire element, including text nested deep into other elements
+            textVal = toISO8859_1(nodeList.item(i).getTextContent());
+            break;
         }
         return textVal;
     }
@@ -32,7 +33,7 @@ abstract public class DomParser {
             return Integer.parseInt(getTextValue(ele, tagName));
         }
         catch (NumberFormatException e){
-            System.out.println("Failed to get integer from the element: " + ele.toString());
+            //System.out.println("Failed to get integer from the element: " + ele.toString());
             return -1;
         }
 
@@ -44,9 +45,22 @@ abstract public class DomParser {
         for (int i = 0; i < nodeList.getLength(); i++){
             Node currentNode = nodeList.item(i).getFirstChild();
             if (currentNode != null){
-                textList.add(currentNode.getNodeValue());
+                textList.add(toISO8859_1(currentNode.getNodeValue()));
             }
         }
         return textList;
+    }
+
+    protected String toISO8859_1(String text)  {
+        try{
+            if (text == null){
+                return "";
+            }
+            return new String(text.getBytes(),"ISO-8859-1");
+        }
+        catch(UnsupportedEncodingException e){
+            return "Should have never happened";
+        }
+
     }
 }
